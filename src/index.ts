07ee3580535f8e188e2b6d16cdc91b3027ca45f1
tuan-nguyen4 <mrbotno@hotmail.com>
2019-08-07 14:10:51 +0700

    Simple router with decorator and metadata
    Create decorators for get method and login controller.
    Bind the router to application.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-console.log("abc");
-console.log("abc");
+import express from "express";
+import { router } from "./controllers/decorators/controller";
+
+import "./controllers/LoginController";
+
+const app = express();
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
