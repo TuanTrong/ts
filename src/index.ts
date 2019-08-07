b40a1f2e3e838a74229059999d1c26b92170d107
tuan-nguyen4 <mrbotno@hotmail.com>
2019-08-07 14:23:17 +0700

    Refactor
    Router singleton
    import decorators.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { router } from "./controllers/decorators/controller";
+import { AppRouter } from "./AppRouter";
 
 import "./controllers/LoginController";
 
 const app = express();
 
-app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
