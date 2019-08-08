40840f1cec3b27f0921c67b2ebf18b4d62ac9ce7
tuan-nguyen4 <mrbotno@hotmail.com>
2019-08-08 09:56:49 +0700

    Generate FE
    npx create-react-app --typescript
    exclude client from be build
    start client and be together

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(AppRouter.getInstance());
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
 });
